f741be036be88b3d0e6b9f10db2c9f4f63ede159
Sayef <syiahmed31@gmail.com>
2025-10-17 20:43:38 -0400

    second commit - updated version for cc

--- src/main/java/ahmed/lab5colorchooser/ColorChooserController.java
+++ src/main/java/ahmed/lab5colorchooser/ColorChooserController.java
@@ -1,84 +1,61 @@
 package ahmed.lab5colorchooser;
 
-// Fig. 13.9: ColorChooserController.java
-// Controller for the ColorChooser app
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.converter.NumberStringConverter;
 
 public class ColorChooserController {
-    // instance variables for interacting with GUI components
+
     @FXML private Slider redSlider;
     @FXML private Slider greenSlider;
     @FXML private Slider blueSlider;
     @FXML private Slider alphaSlider;
+
     @FXML private TextField redTextField;
     @FXML private TextField greenTextField;
     @FXML private TextField blueTextField;
     @FXML private TextField alphaTextField;
+
     @FXML private Rectangle colorRectangle;
 
-    // instance variables for managing
-    private int red = 0;
-    private int green = 0;
-    private int blue = 0;
-    private double alpha = 1.0;
+    @FXML
+    private void initialize() {
 
-    public void initialize() {
-        // bind TextField values to corresponding Slider values
-        redTextField.textProperty().bind(
-                redSlider.valueProperty().asString("%.0f"));
-        greenTextField.textProperty().bind(
-                greenSlider.valueProperty().asString("%.0f"));
-        blueTextField.textProperty().bind(
-                blueSlider.valueProperty().asString("%.0f"));
-        alphaTextField.textProperty().bind(
-                alphaSlider.valueProperty().asString("%.2f"));
+        Bindings.bindBidirectional(redTextField.textProperty(), redSlider.valueProperty(), new NumberStringConverter());
+        Bindings.bindBidirectional(greenTextField.textProperty(), greenSlider.valueProperty(), new NumberStringConverter());
+        Bindings.bindBidirectional(blueTextField.textProperty(), blueSlider.valueProperty(), new NumberStringConverter());
+        Bindings.bindBidirectional(alphaTextField.textProperty(), alphaSlider.valueProperty(), new NumberStringConverter());
 
-        // listeners that set Rectangle's fill based on Slider changes
-        redSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
-                        red = newValue.intValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
-                }
-        );
-        greenSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
-                        green = newValue.intValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
-                }
-        );
-        blueSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
-                        blue = newValue.intValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
-                }
-        );
-        alphaSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
-                        alpha = newValue.doubleValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
-                }
-        );
+
+        redSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        greenSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        blueSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        alphaSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+
+
+        redTextField.textProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        greenTextField.textProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        blueTextField.textProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        alphaTextField.textProperty().addListener((obs, oldVal, newVal) -> updateColor());
+
+
+        updateColor();
+    }
+
+    private void updateColor() {
+        try {
+            double r = redSlider.getValue() / 255.0;
+            double g = greenSlider.getValue() / 255.0;
+            double b = blueSlider.getValue() / 255.0;
+            double a = alphaSlider.getValue();
+
+            colorRectangle.setFill(Color.color(r, g, b, a));
+        } catch (NumberFormatException ex) {
+
+        }
     }
 }
